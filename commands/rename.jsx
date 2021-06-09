31040ed4c43ac8ec8dc24f7260e8b521c0a65755
Glitch (hip-chalk-wombat) <none>
2021-06-09 01:14:35 +0000

    🏍🌧 Checkpoint ./commands/rename.jsx:3188963/193

--- commands/rename.jsx
+++ commands/rename.jsx
@@ -1,1 +1,9 @@
 const Discord = require("discord.js");
+
+module.exports = {
+  name: "clip",
+  description: 'Rename nama orang (DIBUAT SAMA RIZKI)',
+  async execute(message, args) {
+    
+    }
+  }
