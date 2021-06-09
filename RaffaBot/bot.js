4532525d8472f619e16a97040edc5ff5ae681987
RizkiP4BYT <47355568+RizkiP4BYT@users.noreply.github.com>
2021-06-09 20:52:30 +0700

    Create bot.js

--- RaffaBot/bot.js
+++ RaffaBot/bot.js
@@ -2,6 +2,16 @@ const Discord = require("discord.js");
 
 module.exports = {
    RaffaBot_start: function() {
-      
+      bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
+     bot.user.setPresence({
+         game: {
+             name: ``,
+             type: "",
+           state: `${statuslist[random]}`,
+             url: "https://www.twitch.tv/monstercats"
+         },
+     });
+ });
    }
 }
