64af289e348c6ae9f3b9a2c260c23f865142f943
RizkiP4BYT <47355568+RizkiP4BYT@users.noreply.github.com>
2021-06-10 08:24:06 +0700

    Update bot.js

--- RaffaBot/bot.js
+++ RaffaBot/bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const bot = Discord.CLient({disableEveryone: true});
+const bot = Discord.Client({disableEveryone: true});
 const dotenv = require("dotenv");
 dotenv.config();
 
