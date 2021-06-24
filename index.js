68ac54b895d3983eae0f0695d47edc44a9cfac61
raffadian220 <85614812+raffadian220@users.noreply.github.com>
2021-06-24 07:39:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ i18n.configure({
  */
 client.on("ready", () => {
   console.log(`${client.user.username} ready!`);
-  client.user.setActivity(`Subscribe Raffa Dian! | -help`, { type: "STREAMING" });
+  client.user.setActivity(`Subscribe Raffa Dian !! | -help`, { type: "WATCHING" });
 });
 client.on("warn", (info) => console.log(info));
 client.on("error", console.error);
